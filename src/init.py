09cd955a137944f759c1a5545470ef57df5fbc0e
sugrbear <avk639@utexas.edu>
2026-01-06 23:52:25 -0600

    deleted extra files, coded up init files and added gitignore

---
+++ src/init.py
@@ -0,0 +1,11 @@
+"""
+Geometry Learning Package
+"""
+from . import models
+from . import data
+from . import training
+from . import evaluation
+from . import visualization
+from . import utils
+
+__version__ = "0.1.0"
